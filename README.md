02e1d9e2dbb2aebed5d9718f719593289f20ad80
oujoe <josephcaradonna@oakland.edu>
2023-10-29 16:00:17 -0400

    add base files for html, css, and js

---
+++ README.md
@@ -0,0 +1,1 @@
+In Class Assignment #2 CSI 3150 - Joseph Caradonna
---
+++ js/script.js
@@ -0,0 +1,49 @@
+/* Button functions for a, b, and c */
+/* Script to show hidden section on button click from https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp */
+function hiddenA() {
+  var x = document.getElementById("hiddenA");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+
+  var x = document.getElementById("hiddenA2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function hiddenB() {
+  var x = document.getElementById("hiddenB");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+
+  var x = document.getElementById("hiddenB2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function hiddenC() {
+  var x = document.getElementById("hiddenC");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+
+  var x = document.getElementById("hiddenC2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
